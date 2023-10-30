21cc466f24b668d8eb3423afbe37ff1b58a1366b
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-10-31 01:56:03 +0200

    Integrated popular results from db within the `RightSidebar` component

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
+
 import RenderTag from "@/components/shared/RenderTag";
 
 const RightSidebar = async () => {
-  const hotQuestions: any = [];
-  const popularTags: any = [];
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px]">
