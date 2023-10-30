c75788f8ae92295151f0c67d40c60602c2517c0f
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-10-31 01:19:04 +0200

    Implemented dynamic route for tags

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { auth } from "@clerk/nextjs";
+
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+
+import type { URLProps } from "@/types";
+
+const Page = async ({ params }: Omit<URLProps, "searchParams">) => {
+  const { userId: clerkId } = auth();
+
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route={`/tags/${params.id}`}
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              clerkId={clerkId}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="No Tag Questions Found"
+            description="It appears that there are no saved questions in your collection at the moment 😔.Start exploring and saving questions that pique your interest 🌟"
+            link="/"
+            linkTitle="Explore Questions"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,11 +1,15 @@
 "use server";
 
 import Tag from "@/database/tag.model";
+import Question from "@/database/question.model";
+import User from "@/database/user.model";
 
 import { connectToDatabase } from "@/lib/mongoose";
 
-import type { GetAllTagsParams } from "./shared.types";
-import console from "console";
+import type {
+  GetAllTagsParams,
+  GetQuestionByTagIdParams,
+} from "./shared.types";
 
 export async function getAllTags(params: GetAllTagsParams) {
   try {
@@ -14,6 +18,37 @@ export async function getAllTags(params: GetAllTagsParams) {
     const tags = await Tag.find({});
 
     return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId } = params;
+
+    const tag = await Tag.findOne({ _id: tagId }).populate({
+      path: "questions",
+      model: Question,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions };
   } catch (error) {
     console.log(error);
     throw error;
