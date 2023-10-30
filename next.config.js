c9b69bad79e6a5caaea1d04583c5c9d74e8ab519
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-10-31 00:26:48 +0200

    Added image remotePatterns to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
