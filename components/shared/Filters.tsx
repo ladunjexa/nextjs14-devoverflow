1d8f4a270d16d8c19a93712bdb752f7fd7728cf1
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-11-03 14:46:24 +0200

    Modified `Filters` component styles. Renamed remote switch label

--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -56,7 +56,7 @@ const Filters = ({
         <Button
           key={filter.value}
           onClick={() => handleTypeClick(filter.value)}
-          className={`body-medium mb-0.5 mr-0.5 rounded-lg px-6 py-3 capitalize shadow-none md:mb-0 md:mr-0 ${
+          className={`body-medium mb-0.5 mr-0.5 rounded-lg px-6 py-3 capitalize shadow-none md:mb-0 md:mr-0 line-clamp-1 ${
             active === filter.value
               ? "bg-primary-100 text-primary-500 dark:bg-dark-400 dark:hover:bg-dark-400"
               : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
@@ -69,7 +69,7 @@ const Filters = ({
 
       {jobFilter && (
         <div className="background-light800_dark300 mt-2 flex items-center rounded-lg px-3 py-2 shadow-none md:mt-0">
-          <Switcher query="remote" label="Remote Only" />
+          <Switcher query="remote" label="Remote" />
           <Switcher query="wage" label="TBA" />
           <Switcher query="skills" label="Skills" />
         </div>
